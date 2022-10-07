a99e8c0fc778475f4cb25050b94d9aecfe35251a
Vaibhav Arora <vaibhavarora0301vishu@gmail.com>
2022-10-07 17:06:45 +0530

    Navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NavBar from "./components/navbar";
+import NotFound from "./components/notfound";
+import MoviesForm from "./components/moviesform";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MoviesForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
